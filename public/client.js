3702172642c87c7f8b2d5bbaa6c7fdbbf9a094a1
HotDro4illa <3009kartina@gmail.com>
2021-10-09 20:06:19 +0300

    prewiew

--- public/client.js
+++ public/client.js
@@ -1,72 +1,116 @@
-  var socket = io();
-  var log = console.log;
+var socket = io();
+var log = console.log;
 
 
-$(document).ready(function(){
-  
-  var elementX = 0, 
-      elementY = 0,
-      elementW = 0,
-      elementH = 0,
-      mouseX = 0,
-      mouseY = 0;
-  
-$(document).mousemove(function(e){
-  
-   var position = $(".parallax").offset(),
-   obj = $(".parallax");
-   elementX = position.left;
-   elementY = position.top;
-  
-   elementW = obj.width();
-   elementH = obj.height();
-  
-   var halfW = elementW/2;
-   var halfH = elementH/2;
-   
-   mouseX = (e.pageX - elementX - halfW)/halfW;
-   mouseY = (e.pageY - elementY - halfH)/halfH;
-   mouseX = Math.round(mouseX * 100)/200;
-   mouseY = Math.round(mouseY * 100)/200;
-  
-   // console.log(elementX+" "+elementY+" "+halfW+" "+halfH);
-  
-   $(".parallax").css("transform", "rotateX("+mouseY*-10+"deg) rotateY("+mouseX*10+"deg)");
-}); 
-  
+$(document).ready(function() {
+
+    var elementX = 0,
+        elementY = 0,
+        elementW = 0,
+        elementH = 0,
+        mouseX = 0,
+        mouseY = 0;
+
+    $(document).mousemove(function(e) {
+
+        var position = $(".parallax").offset(),
+            obj = $(".parallax");
+        elementX = position.left;
+        elementY = position.top;
+
+        elementW = obj.width();
+        elementH = obj.height();
+
+        var halfW = elementW / 2;
+        var halfH = elementH / 2;
+
+        mouseX = (e.pageX - elementX - halfW) / halfW;
+        mouseY = (e.pageY - elementY - halfH) / halfH;
+        mouseX = Math.round(mouseX * 100) / 200;
+        mouseY = Math.round(mouseY * 100) / 200;
+		
+		if (mouseX > 3.00){
+			mouseX = 3.00;
+		}
+				if (mouseX < -3.00){
+			mouseX = -3.00;
+		}
+		if (mouseY > 1.50) {
+			mouseY = 1.50;
+		}
+				if (mouseY < -1.50) {
+			mouseY = -1.50;
+		}
+
+        // console.log(elementX+" "+elementY+" "+halfW+" "+halfH);
+
+        $(".parallax").css("transform", "rotateX(" + mouseY * -10 + "deg) rotateY(" + mouseX * 10 + "deg)");
+    });
+
 });
 
 
-  
+
 socket.on("catalog_upd", (material) => {
 
-	
-	var inner_vid = "";
-	var inner_img = "";
-	var inner_tik = "";
+    document.querySelector("#stats").innerHTML = "Сейчас архив насчитывает " + material.length + " видео и фото!";
 
 
-	for (var i = 0; i < material.length; i++) {
-		if ((material[i].slice(-1) == "4") && (material[i].slice(-5) != "k.mp4")) {
-		inner_vid += '<video src="https://e-ivettta-files.s3.eu-central-1.amazonaws.com/' + material[i] + '" preload = "none" controls></video>';
-		}
-		if (material[i].slice(-1) == "g") {
-		inner_img += '<img class="image" src="https://e-ivettta-files.s3.eu-central-1.amazonaws.com/' + material[i] + '"></img>';
-		};		
-		if (material[i].slice(-5) == "k.mp4") {
-		inner_tik += '<video src="https://e-ivettta-files.s3.eu-central-1.amazonaws.com/' + material[i] + '" preload = "none" controls></video>';
-		};
-	}
+    var inner_vid = "";
+    var inner_img = "";
+    var inner_tik = "";
+
+    var videos = []
+    var photos = []
+    var tiktok = []
+
+    for (var i = 0; i < material.length; i++) {
+        if ((material[i].slice(-1) == "4") && (material[i].slice(-5) != "k.mp4")) {
+            videos.push(material[i])
+			
+        }
+        if (material[i].slice(-1) == "g") {
+            photos.push(material[i])
+        };
+        if (material[i].slice(-5) == "k.mp4") {
+            tiktok.push(material[i])
+        };
+    }
 	
-	document.querySelector("#videos").innerHTML = inner_vid;
-	document.querySelector("#photos").innerHTML = inner_img;
-	document.querySelector("#tiktok").innerHTML = inner_tik;
-	
-	
-	document.querySelector("#stats").innerHTML = "Сейчас архив насчитывает " + material.length + " видео и фото!";
-	
+
+    window.onscroll = function(ev) {
+        if ((window.innerHeight + window.pageYOffset) >= document.body.offsetHeight - 200) {
+
+            for (var i = 0; i < 7; i++) {
+                if (videos.length > 0) {
+                    inner_vid += '<video src="https://e-ivettta-files.s3.eu-central-1.amazonaws.com/' + videos[i] + '" controls></video>';
+
+                    videos.splice(i, 1);
+                    document.querySelector("#videos").innerHTML = inner_vid;
+                } else {
+                    if (photos.length > 0) {
+
+                        inner_img += '<img class="image" src="https://e-ivettta-files.s3.eu-central-1.amazonaws.com/' + photos[i] + '"></img>';
+                        photos.splice(i, 1);
+                        document.querySelector("#photos").innerHTML = inner_img;
+                    } else {
+                        if (tiktok.length > 0) {
+
+                            inner_tik += '<video src="https://e-ivettta-files.s3.eu-central-1.amazonaws.com/' + tiktok[i] + '" controls></video>';
+                            tiktok.splice(i, 1);
+                            document.querySelector("#tiktok").innerHTML = inner_tik;
+                        }
+                    }
+                }
+
+
+            }
+
+
+
+
+        }
+    };
+
+
 });
-
-
-
-
