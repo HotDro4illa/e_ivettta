3010ada23ed20ccf682c7fd08a3e163d6a4ff006
HotDro4illa <49322236+HotDro4illa@users.noreply.github.com>
2022-08-01 20:37:43 +0300

    upd

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const app = express();
-const http = require('http');
-const server = http.createServer(app);
+const http = require('https');
+const fs = require('fs');
+const options = {
+  key: fs.readFileSync(__dirname+'/secure/key.pem'),
+  cert: fs.readFileSync(__dirname+'/secure/cert.pem')
+};
+const server = http.createServer(options,app);
 const {Server} = require("socket.io");
 const io = new Server(server);
+
 
 const PORT = process.env.PORT || 3000;
 
@@ -13,7 +19,8 @@ const log = console.log;
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+	res.redirect("https://github.com/HotDro4illa/e-ivettta-filehost/tree/master/arch")
+
 });
 
 
