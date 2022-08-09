2d65f988ea68f9512c10a0dfc507dbb77cee4f09
HotDro4illa <49322236+HotDro4illa@users.noreply.github.com>
2022-08-09 09:29:23 +0300

    upd

--- index.js
+++ index.js
@@ -2,17 +2,15 @@ const express = require("express");
 const app = express();
 const http = require("http");
 const fs = require("fs");
-const options = {
-  key: fs.readFileSync(__dirname + "/secure/key.pem"),
-  cert: fs.readFileSync(__dirname + "/secure/cert.pem"),
-};
-const server = http.createServer(options, app);
+const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
 const PORT = process.env.PORT || 3000;
 
 const log = console.log;
+
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
