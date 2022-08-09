440467a1192aa07f33214d7df85a624760246eb1
HotDro4illa <49322236+HotDro4illa@users.noreply.github.com>
2022-08-09 09:35:21 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,12 +5,13 @@ const fs = require("fs");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
+const path = require("path");
 
 const PORT = process.env.PORT || 3000;
 
 const log = console.log;
 
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
